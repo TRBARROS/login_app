2b573fa944edaad02412de36b8c0bd9e5a39aa4d
trbarros <thiago_NO3@outlook.com>
2024-10-13 10:56:53 -0300

    Estrutura montada, iniciar o front da tela de login

--- prj_login/app_login/models.py
+++ prj_login/app_login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class tblusers(models.Model):
+    id = models.IntegerField(primary_key=True)
+    strname = models.CharField(max_length=400)
+    strmidname = models.CharField(max_length=400)
+    strlastname = models.CharField(max_length=400)
+    strlogin = models.CharField(max_length=400)
+    strpassword = models.CharField(max_length=256)
+    stremail = models.CharField(max_length=400)
+    strphone = models.CharField(max_length=20)
+    blnactive = models.BooleanField()
+    dtmupdate = models.DateTimeField()
